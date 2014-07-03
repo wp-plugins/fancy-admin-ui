393f872124d16ec497a97b1354379da563325378
boborchard <boborchard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-03 16:44:09 +0000

    Fixes to the standard adminbar callback from Wordpress core to set heights correctly on the frontend
    git-svn-id: https://plugins.svn.wordpress.org/fancy-admin-ui/trunk@942990 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fancy-admin-ui.php
+++ fancy-admin-ui.php
@@ -32,3 +32,12 @@ function fau_swap_footer_admin() {
   echo 'Custom Admin UI by <a href="http://boborchard.com" target="_blank">Bob Orchard</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a></p>';
 }
 add_filter( 'admin_footer_text', 'fau_swap_footer_admin' );
+
+// Remove default HTML height on the admin bar callback
+function fui_admin_bar_style() { ?>
+  <style type="text/css" media="screen">
+    html { margin-top: 46px !important; }
+    * html body { margin-top: 46px !important; }
+  </style>
+<?php }
+add_theme_support( 'admin-bar', array( 'callback' => 'fui_admin_bar_style' ) );
