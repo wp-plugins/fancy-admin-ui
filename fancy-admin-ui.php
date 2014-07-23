a7dfb243d8e27b2b84936c13f109fb641a84829f
boborchard <boborchard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 02:32:35 +0000

    Changed to tag-based versioning and added ability to set custom colors
    git-svn-id: https://plugins.svn.wordpress.org/fancy-admin-ui/trunk@953177 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fancy-admin-ui.php
+++ fancy-admin-ui.php
@@ -3,27 +3,29 @@
   Plugin Name:  Fancy Admin UI
   Plugin URI:   http://boborchard.com/plugins/fancy-admin-ui/
   Description:  Super clean, blue admin panel theme
-  Version:      0.2
+  Version:      0.3
   Author:       Bob Orchard
   Author URI:   http://boborchard.com
   */
 
+include_once('inc/fau_settings.php');
+
 // Include Admin Styles
 function fau_admin_theme_style() {
-  wp_enqueue_style( 'fau-admin-theme', plugins_url( 'css/fau-styles-admin.css', __FILE__ ) );
-  wp_enqueue_style( 'fau-adminbar', plugins_url( 'css/fau-styles-adminbar.css', __FILE__ ) );
+  wp_enqueue_style( 'fau-admin-theme', plugins_url( 'css/fau-styles-admin.php', __FILE__ ) );
+  wp_enqueue_style( 'fau-adminbar', plugins_url( 'css/fau-styles-adminbar.php', __FILE__ ) );
 }
 add_action( 'admin_enqueue_scripts', 'fau_admin_theme_style' );
 
 function fau_adminbar_style() {
-  wp_enqueue_style( 'fau-admin-theme', plugins_url( 'css/fau-styles-adminbar.css', __FILE__ ) );
+  wp_enqueue_style( 'fau-admin-theme', plugins_url( 'css/fau-styles-adminbar.php', __FILE__ ) );
 }
 add_action( 'wp_enqueue_scripts', 'fau_adminbar_style' );
 
 // Login Page Styling
 function fau_login_theme_style() {
-  wp_enqueue_style( 'fau-login-theme', plugins_url( 'css/fau-styles-login.css', __FILE__ ) );
-  wp_enqueue_script( 'fau-login-theme', plugins_url( 'css/fau-styles-login.css', __FILE__ ) );
+  wp_enqueue_style( 'fau-login-theme', plugins_url( 'css/fau-styles-login.php', __FILE__ ) );
+  wp_enqueue_script( 'fau-login-theme', plugins_url( 'css/fau-styles-login.php', __FILE__ ) );
 }
 add_action( 'login_enqueue_scripts', 'fau_login_theme_style' );
 
