5b18d60e55c288eb527847403885c7499e21f1da
boborchard <boborchard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 12:39:06 +0000

    update to version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/fancy-admin-ui/tags/1.1@1164944 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fancy-admin-ui.php
@@ -0,0 +1,47 @@
+<?php
+/*
+  Plugin Name:  Fancy Admin UI
+  Plugin URI:   http://boborchard.com/plugins/fancy-admin-ui/
+  Description:  Super clean, blue admin panel theme
+  Version:      1.0
+  Author:       Bob Orchard
+  Author URI:   http://boborchard.com
+  */
+
+include_once('inc/fau_settings.php');
+
+// Include Admin Styles
+function fau_admin_theme_style() {
+  wp_enqueue_style( 'fau-admin-theme', plugins_url( 'css/fau-styles-admin.php', __FILE__ ) );
+  wp_enqueue_style( 'fau-adminbar', plugins_url( 'css/fau-styles-adminbar.php', __FILE__ ) );
+}
+add_action( 'admin_enqueue_scripts', 'fau_admin_theme_style' );
+
+function fau_adminbar_style() {
+  wp_enqueue_style( 'fau-admin-theme', plugins_url( 'css/fau-styles-adminbar.php', __FILE__ ) );
+}
+add_action( 'wp_enqueue_scripts', 'fau_adminbar_style' );
+
+// Login Page Styling
+function fau_login_theme_style() {
+  wp_enqueue_style( 'fau-login-theme', plugins_url( 'css/fau-styles-login.php', __FILE__ ) );
+  wp_enqueue_script( 'fau-login-theme', plugins_url( 'css/fau-styles-login.php', __FILE__ ) );
+}
+add_action( 'login_enqueue_scripts', 'fau_login_theme_style' );
+
+// Update Admin Footer
+function fau_swap_footer_admin() {
+  echo 'Custom Admin UI by <a href="http://boborchard.com" target="_blank">Bob Orchard</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a></p>';
+}
+add_filter( 'admin_footer_text', 'fau_swap_footer_admin' );
+
+// Remove default HTML height on the admin bar callback
+function fui_admin_bar_style() {
+  if ( is_admin_bar_showing() ) {
+?>
+  <style type="text/css" media="screen">
+    html { margin-top: 46px !important; }
+    * html body { margin-top: 46px !important; }
+  </style>
+<?php } }
+add_theme_support( 'admin-bar', array( 'callback' => 'fui_admin_bar_style' ) );
